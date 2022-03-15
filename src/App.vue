f566aa7ebc7825c76898725dea9fb2cf8ce913f2
dymafr <erwan1309@hotmail.com>
2022-03-15 13:01:44 +0100

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,41 @@
 <template>
-  <input v-on="validationListeners" v-model="usernameValue" type="text" />
-  <p v-if="usernameError">{{ usernameError }}</p>
-
-  <pre>
-    {{ usernameValue }}
-  </pre>
+  <form>
+    <input v-model="passwordValue" type="password" placeholder="Mot de passe" />
+    <input
+      v-model="validatePasswordValue"
+      @blur="handleChange"
+      type="password"
+      placeholder="Vérifier le mot de passe"
+    />
+    <p v-if="confirmPasswordError">{{ confirmPasswordError }}</p>
+  </form>
 </template>
 
 <script setup lang="ts">
 import { useForm, useField } from 'vee-validate';
 import { z } from 'zod';
-import { computed } from 'vue';
 import { toFormValidator } from '@vee-validate/zod';
 
-const validationSchema = z.object({
-  username: z
-    .string()
-    .min(3, { message: 'Le champ est trop court' })
-    .max(10, { message: 'Le champ est trop long' }),
-});
-
-const validationListeners = computed(() => {
-  //  Validation douce si pas encore d'erreur :
-  if (!usernameError.value) {
-    return {
-      blur: handleChange,
-      change: handleChange,
-    };
-  } else {
-    //  Validation agressive en cas d'erreur :
-    return {
-      blur: handleChange,
-      change: handleChange,
-      input: handleChange,
-    };
-  }
-});
+const validationSchema = z
+  .object({
+    password: z.string(),
+    validatePassword: z.string(),
+  })
+  .refine((data) => data.password === data.validatePassword, {
+    path: ['validatePassword'],
+    message: 'Les mots de passe ne correspondent pas',
+  });
 
 useForm({
   validationSchema: toFormValidator(validationSchema),
-  initialValues: { username: 'Test' },
 });
 
+const { value: passwordValue } = useField('password');
 const {
-  value: usernameValue,
-  errorMessage: usernameError,
+  value: validatePasswordValue,
   handleChange,
-} = useField('username', null, { validateOnValueUpdate: false });
+  errorMessage: confirmPasswordError,
+} = useField('validatePassword', null, { validateOnValueUpdate: false });
 </script>
 
 <style scoped lang="scss"></style>
